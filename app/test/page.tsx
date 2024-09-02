a85972f27e26022568f06e4ba8f5d40dd348c7f3
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-09-02 22:39:26 +0700

    add convex auth

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-
 export default function Test() {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </main>
-  );
+  return <div>TEST PAGE</div>;
 }
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"]);
+
+export default convexAuthNextjsMiddleware((request) => {
+  if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+  // REDIRECT USER FROM SIGN IN PAGE TO AUTHENTICATED PAGE
+});
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
