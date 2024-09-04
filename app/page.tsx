bd608907619b1851f67339e16b3a8719c3ca2fe6
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-09-04 10:33:56 +0700

    add user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import UserButton from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      <div>Logged IN</div>
-      <Button onClick={() => signOut()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
