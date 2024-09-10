2d824dffe08715862107a641c753941c54cb2930
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-09-10 10:43:38 +0700

    add sidebar item

--- app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -3,8 +3,14 @@
 import useCurrentMember from "@/features/members/api/use-current-member";
 import useGetWorkspace from "@/features/workspaces/api/use-get-workspace";
 import useWorkspaceId from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
+import {
+  AlertTriangle,
+  Loader,
+  MessageSquareText,
+  SendHorizonal,
+} from "lucide-react";
 import WorkspaceHeader from "./workspace-header";
+import SidebarItem from "./sidebar-item";
 
 export default function WorkspaceSidebar() {
   const workspaceId = useWorkspaceId();
@@ -37,6 +43,10 @@ export default function WorkspaceSidebar() {
         workspace={workspace}
         isAdmin={member.role === "admin"}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
+      </div>
     </div>
   );
 }
