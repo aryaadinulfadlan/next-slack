bbdba68b6a16796c6ed9acb8f0df07778b3a788d
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-09-23 10:46:22 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-AUTH_GITHUB_SECRET=d8f3cab66c4aedb77798d43ffa45d157a0e86cb8
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+### Demo App: https://next-slack-like.vercel.app
+
+## Tech Stack:
+
+1. Next JS 14.2.7
+2. Fully TypeScript
+3. Convex Authentication
+4. Convex Realtime Database
+5. Tailwind CSS
+6. Shadcn UI
+7. Jotai
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const { replace } = useRouter();
@@ -22,8 +23,8 @@ export default function Home() {
 
   // console.log({ workspaceId, data });
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
